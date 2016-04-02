9b3f92d8d03f42eb09d31c4a406fa4cfa4b06ffd
Henry Mortimer <henry@morti.plus.com>
2016-04-02 17:18:26 +0100

    wrote some stuff that doenst actually do anything

--- part2/src/comp207p/main/ConstantFolder.java
+++ part2/src/comp207p/main/ConstantFolder.java
@@ -16,7 +16,9 @@ import org.apache.bcel.generic.InstructionList;
 import org.apache.bcel.util.InstructionFinder;
 import org.apache.bcel.generic.MethodGen;
 import org.apache.bcel.generic.TargetLostException;
-
+import org.apache.bcel.generic.LDC;
+import org.apache.bcel.generic.ICONST;
+import org.apache.bcel.generic.IADD;
 
 
 public class ConstantFolder
@@ -30,6 +32,7 @@ public class ConstantFolder
 	public ConstantFolder(String classFilePath)
 	{
 		try{
+      System.out.println(classFilePath);
 			this.parser = new ClassParser(classFilePath);
 			this.original = this.parser.parse();
 			this.gen = new ClassGen(this.original);
@@ -37,6 +40,67 @@ public class ConstantFolder
 			e.printStackTrace();
 		}
 	}
+
+  private void optimizeMethod(ClassGen cgen, ConstantPoolGen cpgen, Method method)
+  {
+    // Get the Code of the method, which is a collection of bytecode instructions
+		Code methodCode = method.getCode();
+
+		// Now get the actualy bytecode data in byte array, 
+		// and use it to initialise an InstructionList
+		InstructionList instList = new InstructionList(methodCode.getCode());
+
+		// Initialise a method generator with the original method as the baseline	
+		MethodGen methodGen = new MethodGen(method.getAccessFlags(), method.getReturnType(), method.getArgumentTypes(), null, method.getName(), cgen.getClassName(), instList, cpgen); 
+		InstructionHandle h1 = null, h2 = null;
+		//InstructionHandle is a wrapper for actual Instructions
+		for(InstructionHandle handle : instList.getInstructionHandles())
+    {
+      if(handle.getInstruction() instanceof LDC){
+        System.out.println(((LDC)handle.getInstruction()).getValue(cpgen));
+      } 
+      if(h1 != null && h2 != null)
+      {
+        if(handle.getInstruction() instanceof  IADD &&
+            h1.getInstruction() instanceof LDC &&
+            h2.getInstruction() instanceof LDC )
+        {
+          int value = ((Integer)(((LDC)h1.getInstruction()).getValue(cpgen))+ (Integer)(((LDC)h2.getInstruction()).getValue(cpgen)));
+          
+          instList.insert(handle, new LDC(cpgen.addInteger(value)));
+          System.out.println("here");
+          try
+          {
+            // delete the old one
+            instList.delete(handle);
+            instList.delete(h1);
+            instList.delete(h2);  
+            System.out.println("here too");
+          }
+          catch (TargetLostException e)
+          {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          }
+        }
+      }
+      h2 = h1;
+      h1 = handle;
+    }
+		// setPositions(true) checks whether jump handles 
+		// are all within the current method
+		instList.setPositions(true);
+
+		// set max stack/local
+		methodGen.setMaxStack();
+		methodGen.setMaxLocals();
+
+		// generate the new method with replaced iconst
+		Method newMethod = methodGen.getMethod();
+		// replace the method in the original class
+		cgen.replaceMethod(method, newMethod);
+    System.out.println("here finally");
+  }
 	
 	public void optimize()
 	{
@@ -45,6 +109,12 @@ public class ConstantFolder
 
 		// Implement your optimization here
         
+    Method[] methods = cgen.getMethods();
+
+    for(Method m : methods)
+    {
+      optimizeMethod(cgen, cpgen, m);
+    }
 		this.optimized = gen.getJavaClass();
 	}
 
